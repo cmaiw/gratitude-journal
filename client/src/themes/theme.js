2253c73ed7e6352e16c301d6e43c99400431fa44
cmaiw <ch.maiwald80@gmail.com>
2019-12-19 17:44:55 +0100

    Changed wrong hexcolor in theme

---
+++ client/src/themes/theme.js
@@ -0,0 +1,13 @@
+const defaultTheme = {
+  colors: {
+    background: '#F8F8EF',
+    font: '#2F2314',
+    primary: '#2B6272', //background landing page and most headings
+    secondary: '#2F2314', //background forms and textareas
+    tertiary: '#ED9027', //buttons navbar
+    quartenary: '#B7DD96', //buttons hover state
+    quinary: '#63A4B7' //submit confirmation textarea
+  }
+};
+
+export default defaultTheme;
