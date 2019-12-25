92d0c7980cc42874ea75f95de33f22378e5a25de
cmaiw <ch.maiwald80@gmail.com>
2019-12-25 01:13:18 +0100

    Fixed deepscan issue in storybook file

--- client/src/stories/4-Button.stories.js
+++ client/src/stories/4-Button.stories.js
@@ -7,8 +7,6 @@ export default {
   title: 'Universal Button (edit, back, delete, submit)'
 };
 
-export const text = () => <UniversalButton onClick={action('clicked')}>submit</UniversalButton>;
-
 export function MyUniversalButton() {
   return (
     <UniversalButton onClick={action('clicked')} text={text('Button Label', 'edit')}>
