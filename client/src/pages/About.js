f3c789f1c16802e14d0677a7f17fb6b88a9a9eb3
cmaiw <ch.maiwald80@gmail.com>
2019-12-28 21:58:18 +0100

    Add About Article

---
+++ client/src/pages/About.js
@@ -0,0 +1,92 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import UniversalButton from '../components/UniversalButton';
+
+const AboutTitle = styled.h2`
+  color: ${props => props.theme.colors.primary};
+  font-family: 'Cookie', cursive;
+  margin-top: 50px;
+  margin-right: auto;
+  margin-left: auto;
+  display: block;
+  width: 85%;
+`;
+
+const AboutPageWrapper = styled.div`
+width: 100%;
+height: 100%;
+display: flex:
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin: auto;
+font-size: 12px;
+`;
+
+const ArticleWrapper = styled.div`
+width: 85%;
+height: auto;
+display: flex:
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin: auto;
+font-size: 12px;
+background-color: ${props => props.theme.colors.secondary};
+padding: 10px;
+border-radius: 8px;
+opacity: 0.75;
+`;
+
+const AboutMainArticle = styled.main`
+  display: block;
+  width: 85%;
+  justify-content: flex-start;
+  align-items: center;
+  flex-wrap: wrap;
+  overflow: scroll;
+  color: ${props => props.theme.colors.font};
+  margin: auto;
+  margin-top: 10px;
+`;
+
+const NavWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 30px;
+`;
+function About() {
+  return (
+    <AboutPageWrapper>
+      <AboutTitle>About:</AboutTitle>
+      <ArticleWrapper>
+        <AboutMainArticle>
+          Welcome! This journal is about becoming happier by focusing on the positive aspects in
+          your life.
+        </AboutMainArticle>
+        <AboutMainArticle>
+          It happens to all of us. We think about the day and focus on the cold, rainy weather, a
+          loud unfriendly person on our way to work or our train on delay.
+        </AboutMainArticle>
+        <AboutMainArticle>
+          But what about the little wonders- the small things during our day that make us smile.
+          Someone, keeps the door open for us, smiles at us, we can laugh with our colleagues at
+          lunch break, we find a beautiful colored leaf in the cold autumn weather and it makes us
+          smile.
+        </AboutMainArticle>
+        <AboutMainArticle>
+          This journal will help you to remember people and things you love, your smiles and
+          laughter during the day and it will help you to be thankful for the hidden benefits in
+          your life. By using the journal on a daily basis you will become more grateful, healthier
+          and happier.
+        </AboutMainArticle>
+      </ArticleWrapper>
+      <NavWrapper>
+        <UniversalButton>Back</UniversalButton>
+      </NavWrapper>
+    </AboutPageWrapper>
+  );
+}
+
+export default About;
---
+++ client/src/stories/8-About.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import About from '../pages/About';
+
+export default {
+  title: 'About Article'
+};
+
+export function AboutArticle() {
+  return <About></About>;
+}
