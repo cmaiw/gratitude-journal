b2802e45b99f351ebd15f5fe296360b98e3bffd8
cmaiw <ch.maiwald80@gmail.com>
2020-01-05 15:35:52 +0100

    Correct deepscan issues after  inserting wrong code by accident

--- client/src/stories/7-Form.stories.js
+++ client/src/stories/7-Form.stories.js
@@ -8,12 +8,3 @@ export default {
 export function EntryInputFormJournal() {
   return <EntryInputFormNewEntryByDate></EntryInputFormNewEntryByDate>;
 }
-import React from 'react';
-import EntryInputFormNewEntryByDate from '../pages/EntryInputForm';
-
-export default {
-  title: 'Entry Input Form'
-};
-
-export function EntryInputFormJournal() {
-  return <EntryInputFormNewEntryByDate></EntryInputFormNewEntryByDate>;
