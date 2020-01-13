d708eadf8acd2494c6f8c8db441d0ad62974a4ba
cmaiw <ch.maiwald80@gmail.com>
2020-01-13 13:00:59 +0100

    Fix scripts

---
+++ client/src/stories/11-Confirmation.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Confirmation from '../pages/Confirmation';
+
+export default {
+  title: 'Entry Confirmation Page'
+};
+
+export function JournalConfirmation() {
+  return <Confirmation></Confirmation>;
+}
