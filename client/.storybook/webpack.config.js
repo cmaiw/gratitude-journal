81538dbbc9b04f03f4a96685101c1ffb5dd4faae
cmaiw <ch.maiwald80@gmail.com>
2020-08-25 12:54:40 +0200

    Fixed issues with storybook deployment

--- client/.storybook/webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = async ({ config, mode }) => {
-  if (mode === 'PRODUCTION') {
-    // Change public path to allow deployment into a subpath like https://example.com/storybook
-    config.output.publicPath = '/storybook/';
-  }
-  return config;
-};
